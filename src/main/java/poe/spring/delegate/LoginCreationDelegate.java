57ec42c5216b734f55ba49c3ce7b7e7b2d640a0d
unknown <poidevin.guillaume@gmail.com>
2018-02-26 17:11:19 +0100

    loginCreationDelegate.java - static final String[] FORBIDDEN_LOGIN = { toto, tata, titi }; - from public - to protected

--- src/main/java/poe/spring/delegate/LoginCreationDelegate.java
+++ src/main/java/poe/spring/delegate/LoginCreationDelegate.java
@@ -4,7 +4,7 @@ public class LoginCreationDelegate {
 
 	public static final int MIN_LOGIN_LENGTH = 4;
 	public static final int MAX_LOGIN_LENGTH = 10;
-	public static final String[] FORBIDDEN_LOGIN = { "toto", "tata", "titi" };
+	protected static final String[] FORBIDDEN_LOGIN = { "toto", "tata", "titi" };
 
 	public static Boolean isSizeValid(String login) {
 		Boolean isValid;
