42a9927c36fc29a0c1002182fbf483e6a161b848
unknown <poidevin.guillaume@gmail.com>
2018-02-26 14:54:03 +0100

    init commit

---
+++ src/main/java/poe/spring/Application.java
@@ -0,0 +1,17 @@
+package poe.spring;
+
+import java.math.BigDecimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		 SpringApplication.run(Application.class, args);
+
+	}
+
+
+}
---
+++ src/main/java/poe/spring/annotation/ChronoAspect.java
@@ -0,0 +1,35 @@
+package poe.spring.annotation;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ChronoAspect {
+
+    @Pointcut("execution(public * *(..))")
+    public void publicMethod() {
+    }
+
+
+    @Around("@annotation(poe.spring.annotation.Chrono)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return proceed;
+    }
+
+    @Around("@within(poe.spring.annotation.Chrono)")
+    public Object logExecutionTimeClass(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+        System.out.println(joinPoint.getSignature() + " executed class in " + executionTime + "ms");
+        return proceed;
+    }
+}
