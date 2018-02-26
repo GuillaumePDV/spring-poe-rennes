e72d543612d48c41ff787bb5b6934fc4e37c4934
unknown <poidevin.guillaume@gmail.com>
2018-02-26 18:21:50 +0100

    correcting springboot.src.main.java.poe.spring.annotation.ChronoAspect.java

--- src/main/java/poe/spring/Application.java
+++ src/main/java/poe/spring/Application.java
@@ -1,7 +1,5 @@
 package poe.spring;
 
-import java.math.BigDecimal;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,9 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		 SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
 
 	}
-
 
 }
--- src/main/java/poe/spring/annotation/ChronoAspect.java
+++ src/main/java/poe/spring/annotation/ChronoAspect.java
@@ -1,5 +1,7 @@
 package poe.spring.annotation;
 
+import java.util.logging.*;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -10,26 +12,39 @@ import org.springframework.stereotype.Component;
 @Component
 public class ChronoAspect {
 
-    @Pointcut("execution(public * *(..))")
-    public void publicMethod() {
-    }
+	@Pointcut("execution(public * *(..))")
+	public void publicMethod() {
+	}
 
+	@Around("@annotation(poe.spring.annotation.Chrono)")
+	public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.currentTimeMillis();
+		Object proceed = joinPoint.proceed();
+		long executionTime = System.currentTimeMillis() - start;
+		System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
 
-    @Around("@annotation(poe.spring.annotation.Chrono)")
-    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-        long start = System.currentTimeMillis();
-        Object proceed = joinPoint.proceed();
-        long executionTime = System.currentTimeMillis() - start;
-        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
-        return proceed;
-    }
+		return proceed;
+	}
 
-    @Around("@within(poe.spring.annotation.Chrono)")
-    public Object logExecutionTimeClass(ProceedingJoinPoint joinPoint) throws Throwable {
-        long start = System.currentTimeMillis();
-        Object proceed = joinPoint.proceed();
-        long executionTime = System.currentTimeMillis() - start;
-        System.out.println(joinPoint.getSignature() + " executed class in " + executionTime + "ms");
-        return proceed;
-    }
+	@Around("@within(poe.spring.annotation.Chrono)")
+	public Object logExecutionTimeClass(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.currentTimeMillis();
+		Object proceed = joinPoint.proceed();
+		long executionTime = System.currentTimeMillis() - start;
+		System.out.println(joinPoint.getSignature() + " executed class in " + executionTime + "ms");
+		return proceed;
+	}
+
+	// Logger
+	// logger=Logger.getLogger(".springboot.src.main.java.poe.spring.annotation.ChronoAspect.java");
+	// protected static Logger logger=
+	// Logger.getLogger("myPackage.mySubPackage.myClasse");
+	protected void logg(String s) {
+		Logger logger = Logger.getLogger(".springboot.src.main.java.poe.spring.annotation.ChronoAspect.java");
+		/*
+		 * http://cyberzoide.developpez.com/tutoriels/java/logging/
+		 * */
+//		Handler fh = newFileHandler("ChronoAspect.log");
+//		logger.addHandler(fh);
+	}
 }
