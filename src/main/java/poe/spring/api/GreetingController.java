8227cbf7ab99f5dbb2645849f3529dee9a9c1cc8
unknown <poidevin.guillaume@gmail.com>
2018-02-27 17:01:32 +0100

    comment

--- src/main/java/poe/spring/api/GreetingController.java
+++ src/main/java/poe/spring/api/GreetingController.java
@@ -2,7 +2,6 @@ package poe.spring.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,13 +12,11 @@ import poe.spring.domain.Greeting;
 @RestController
 public class GreetingController {
 	
-	
-	private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(GreetingController.class);
 	
 	@RequestMapping("/hello")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		String logg = String.format("hello here");
-		logger.debug(logg);
+		LOGGER.debug("hello here");
 		return new Greeting(1, "hello " + name);
 	}
 
